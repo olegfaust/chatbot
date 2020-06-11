15f3e9894ce0d1dd7e1cc0d5c42d9a40baab8702
olegfaust <faust.ru@gmail.com>
2020-06-12 00:12:34 +0300

    fixed example_index.sh

--- example_index.sh
+++ example_index.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH="$PWD"
 
 if command -v "python3" >/dev/null; then
-    python3 scripts/build_index.py
+    python3 scripts/build_index.py data/small.csv
 else
-    python scripts/build_index.py
+    python scripts/build_index.py data/small.csv
 fi
